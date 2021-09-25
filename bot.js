4cd0e67eabac41333c062b6c20c3b5697106415e
Pedro Pazello <email@pedropazello.com>
2021-09-25 10:50:13 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,40 @@
+function clickOnCookie(){
+  $("#bigCookie").click()
+  setTimeout(clickOnCookie,1)
+}
+
+function buyResources() {
+	var cursor = $("#product0")
+	var grandma = $("#product1")
+  var farm = $("#product2")
+  var mine = $("#product3")
+  var factory = $("#product4")
+  var bank = $("#product5")
+  var temple = $("#product6")
+  var wizardTower = $("#product7")
+  var shipment = $("#product8")
+  var alchemyLab = $("#product9")
+
+  var productsEnabled = [
+  ]
+
+  productsEnabled.forEach(function (product) {
+    product.click()
+  })
+
+	setTimeout(buyResources, 1000);
+}
+
+function clickOnRandomCookies() {
+  if ($(".shimmer")) {
+    $(".shimmer").click()
+  }
+  setTimeout(clickOnRandomCookies,1)
+}
+
+var concurrentClickers = 100
+
+
+clickOnCookie()
+buyResources()
+clickOnRandomCookies()
